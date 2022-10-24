ed51374f362a0965a2ccc8bfa708afa957bd0a62
xiaolong <123@qq.com>
2022-10-24 14:22:10 +0800

    demo7-装饰器语法的安装和配置

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
     render() {
         return (
-            <div>App组件-demo6</div>
+            <div>App组件-demo7</div>
         )
     }
 }
--- src/one.js
+++ src/one.js
@@ -1,3 +1,13 @@
 import two from "./two"
 const x = 3;
 console.log(x + two.y);
+// 装饰器语法  @testtable 代表的是 立即调用函数 传入的第一个参数应为是 类 
+function testtable(target) {
+    target.isShow = true // 在类上添加属性
+}
+// 
+@testtable
+class Mytest {
+
+}
+console.log("装饰器语法", Mytest.isShow)
